dc8fce4223c181175749e3802d1f4f1bb4ddef8f
Pedro Luis Sanchez Calle <psanchezc09@gmail.com>
2022-09-16 12:33:23 -0500

    Vista del proyecto

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desarrollador PHP Junior</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.css" />
+    <link rel="stylesheet" href="views/index.css">
+</head>
+<body>
+    <button type="button" class="btn btn-primary" id="getDatos">Listar datos</button>
+    <div id="datos">
+        <table id="tablaQuery" class="table table-striped table-bordered table-condensed">
+            <thead class="text-center">
+                <tr>
+                    <th>Id</th>
+                    <th>Contact_no</th>
+                    <th>Lastname</th>
+                    <th>Createdtime</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.js"></script>
+    <script src=" https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="views/index.js"></script>
+</body>
+</html>
