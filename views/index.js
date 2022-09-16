52e32057c691a7100ae1cf9e3699831e845ecdbe
Pedro Luis Sanchez Calle <psanchezc09@gmail.com>
2022-09-16 12:34:29 -0500

    Interacción a la vista

---
+++ views/index.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+  /**
+   * Al dar click llama la función query()
+   */
+  $("#getDatos").click(() => {
+    /**
+     * Muestra un mensaje de espera en pantalla
+     */
+    swal({
+      title: "Cargando...",
+      text: "Espere unos segundos!",
+      icon: "warning",
+      timer: 3000,
+      showConfirmButton: false,
+    });
+
+    query();
+  });
+
+  /**
+   * Realiza la petición query y llena la tabla con los datos recibidos
+   */
+  function query() {
+    $.get("controllers/pruebaController.php?op=query", function (data) {
+      data = JSON.parse(data);
+      console.log("Datos", data.data);
+      tablaDatos = $("#tablaQuery").DataTable({
+        destroy: true,
+        data: data.data,
+        columns: [
+          { data: "id" },
+          { data: "contact_no" },
+          { data: "lastname" },
+          { data: "createdtime" },
+        ],
+        language: {
+          sProcessing: "Procesando...",
+          sLengthMenu: "Mostrar _MENU_ registros",
+          sZeroRecords: "No se encontraron resultados",
+          sEmptyTable: "Ningún dato disponible en esta tabla",
+          sInfo: "Mostrando un total de _TOTAL_ registros",
+          sInfoEmpty: "Mostrando un total de 0 registros",
+          sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
+          sInfoPostFix: "",
+          sSearch: "Buscar:",
+          sUrl: "",
+          sInfoThousands: ",",
+          sLoadingRecords: "Cargando...",
+          oPaginate: {
+            sFirst: "Primero",
+            sLast: "Último",
+            sNext: "Siguiente",
+            sPrevious: "Anterior",
+          },
+        },
+      });
+    });
+  }
+});
